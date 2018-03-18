a23a6af4b360d0f1d0f7586214e332a8692eb283
Shalika-Madhushanki <shali.madhushanki@gmail.com>
2018-03-18 23:30:01 +0530

    change files

--- login.php
+++ login.php
@@ -5,11 +5,11 @@ $username= $_POST["username"];
 $password=$_POST["password"];
 //echo "pswwwwwwwwwwwwwww"+$password;
 
-//$sth= "SELECT * FROM users WHERE username = '$username' and password= '$password' ";
-//$result=mysqli_query($connection,$sth);
-/*if(mysqli_num_rows($result)>0){
+$sth= "SELECT * FROM users WHERE username = '$username' and password= '$password' ";
+$result=mysqli_query($connection,$sth);
+if(mysqli_num_rows($result)>0){
 	echo "login successful !";
 }else{
 	echo "login failed !";
-}*/
+}
 ?> 
