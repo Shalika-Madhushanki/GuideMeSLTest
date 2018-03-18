da955b717f22648363fb9c7ca22024c59b70a69c
Shalika-Madhushanki <shali.madhushanki@gmail.com>
2018-03-18 14:43:55 +0530

    change files

--- connect.php
+++ connect.php
@@ -4,4 +4,10 @@ $mysql_username = "shaliadmin";
 $mysql_password="shaliadmin";
 $mysql_server="db4free.net";
 $connection=mysqli_connect($mysql_server, $mysql_username, $mysql_password,$db_name);
+if($connection){
+	echo "conection successful";
+	
+}else{
+	echo "connection failed";
+}
 ?>
