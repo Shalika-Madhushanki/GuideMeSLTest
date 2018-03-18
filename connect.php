eff85b17e5d4358df0f673f9528ab8d185f30f57
Shalika-Madhushanki <shali.madhushanki@gmail.com>
2018-03-18 12:59:34 +0530

    change files

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-$db_name="GuideME_SL";
-$mysql_username = "root";
-$mysql_password="";
-$mysql_server="localhost";
+$db_name="guidemesldb";
+$mysql_username = "shaliadmin";
+$mysql_password="shaliadmin";
+$mysql_server="db4free.net";
 $connection=mysqli_connect($mysql_server, $mysql_username, $mysql_password,$db_name);
 ?>
