11b4dd656117ecf3b2abd74fe633c6cf4ee3b2a3
Shalika-Madhushanki <shali.madhushanki@gmail.com>
2018-03-18 22:30:12 +0530

    add insert.php file

---
+++ insert.php
@@ -0,0 +1,10 @@
+<?php
+require "connect.php";
+$query= "insert into users (username, password) values (shalika,shalika)";
+if($connection->query($query) === TRUE){
+    echo "Data inserted successfully !";
+}else{
+    echo " Error: ". $connection->error;
+}
+$connection->close();
+?>
