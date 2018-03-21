58ca37af9a0f0ae6c3105ffaa888db72c2d31cc1
Shalika-Madhushanki <shali.madhushanki@gmail.com>
2018-03-21 22:03:48 +0530

    change files

--- addAccomPlaces.php
+++ addAccomPlaces.php
@@ -1,15 +1,15 @@
 <?php
 require "connect.php";
-$place_name = $_POST[""];
-$description = $_POST[""];
+$place_name = $_POST["place_name"];
+$description = $_POST["description"];
 $latitude = $_POST[""];
 $longitude = $_POST[""];
-$charges = $_POST[""];
-$contact_no = $_POST[""];
+$charges = $_POST["charges"];
+$contact_no = $_POST["contact_no"];
 $query= "insert into accommodation (place_name, description, latitude, longitude, charges, contact_no ) values ($place_name, $description, $latitude, $longitude, $charges, $contact_no)";
 
 if($connection->query($query) === TRUE){
-    echo "Data inserted successfully !";
+    echo "data inserted successfully !";
 }else{
     echo " Error: ". $connection->error;
 }
